fc861e521192b48a485bfdaa429ba71806753cfb
Vandeportaele Bertrand <bvandepo@laas.fr>
2016-01-18 22:26:23 +0100

    dot2png.sh fixes

--- scripts/dot2png.sh
+++ scripts/dot2png.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
  
-echo usage: ./dot2png.sh fichier.fsm   
+echo usage: ./dot2png.sh fichier.dot 
 echo ----------------------COMPUTING FILE NAMES----------------------
-dotfile=`echo "${1/.fsm/.dot}"`
+dotfile=`echo "${1}"`
 echo $dotfile
-pngfile=`echo "${1/.fsm/.png}"`
+pngfile=`echo "${1/.dot/.png}"`
 echo $pngfile
 
 dot -Tpng $dotfile -o $pngfile
+display $pngfile 
 
